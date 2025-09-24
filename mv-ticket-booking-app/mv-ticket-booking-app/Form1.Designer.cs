163c8b57f634f995130906fce31ebef03d763bb4
khanhhoilm <khanhhoilm@gmail.com>
2025-09-24 11:26:42 +0700

    add login form

--- mv-ticket-booking-app/mv-ticket-booking-app/Form1.Designer.cs
+++ mv-ticket-booking-app/mv-ticket-booking-app/Form1.Designer.cs
@@ -28,13 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TextBox txtUsername;
+            this.lbUsername = new System.Windows.Forms.Label();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            txtUsername = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbUsername
+            // 
+            this.lbUsername.AutoSize = true;
+            this.lbUsername.Location = new System.Drawing.Point(116, 66);
+            this.lbUsername.Name = "lbUsername";
+            this.lbUsername.Size = new System.Drawing.Size(107, 25);
+            this.lbUsername.TabIndex = 0;
+            this.lbUsername.Text = "username";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new System.Drawing.Point(106, 105);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new System.Drawing.Size(544, 31);
+            txtUsername.TabIndex = 1;
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Location = new System.Drawing.Point(116, 170);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(106, 25);
+            this.lbPassword.TabIndex = 2;
+            this.lbPassword.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(106, 208);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(544, 31);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(257, 296);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(230, 69);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Log in";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(txtUsername);
+            this.Controls.Add(this.lbUsername);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbUsername;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
--- mv-ticket-booking-app/mv-ticket-booking-app/Form1.cs
+++ mv-ticket-booking-app/mv-ticket-booking-app/Form1.cs
@@ -16,5 +16,12 @@ namespace mv_ticket_booking_app
         {
             InitializeComponent();
         }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            //
+            ///
+            //
+        }
     }
 }
